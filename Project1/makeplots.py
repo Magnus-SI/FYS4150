689aaee7e6b084a4ae0091142531271715021f6a
Magnus-SI <40592328+Magnus-SI@users.noreply.github.com>
2020-09-03 15:36:37 +0200

    some changes

--- Project1/makeplots.py
+++ Project1/makeplots.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 plt.figure()
-for N in ('10', '100', '1000'):
-    df = pd.read_csv('N' + N + '.csv')
+for N in ('1', '2', '3'):
+    df = pd.read_csv('values' + N + '.csv')
     x = df['x'].values
     v = df['v'].values
     plt.plot(x, v, label = 'N = %s'%N)
--- Project1/poissonsol.cpp
+++ Project1/poissonsol.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <iomanip>
 #include <math.h>
+//#include <ctime>
+#include "time.h"
 using namespace std;
 
 ofstream ofile;
@@ -82,29 +84,67 @@ double compute_eps(int n, double *u_array, double *v_array){
   cout << "n = " << n << endl;
 	cout << "Max Relative error eps_max: " << eps_max << endl;
   cout << "log10(eps_max) = " << log10(eps_max) << endl;
-  return 0;
+  return log10(eps_max);
 	}
 
 int main(int argc, char* argv[]){
-  int N = atoi(argv[1]);
-  char *ofilename;
-  ofilename = argv[2];
-
-  init(N);      //initialize constantsreturn
-  double *gt = new double[N];
-  double *dt = new double[N];
-  double *v = new double[N];
-  forward_general(gt, dt, N);
-  backward_general(v, gt, dt, N);
-
-  ofile.open(ofilename);
-  ofile << "x," << "u," << "v" << endl;
-  for (int i = 0; i<N; i++){
-    ofile << setw(15) << setprecision(8) << x[i] <<"," << u[i] << "," << v[i] << endl;
+  if (!argv[2]){
+    cout << "Call program with exponent and method choice (0 or 1)" <<endl;
+    exit(0);
   }
-  ofile.close();
 
-  compute_eps(N, u, v);
+  int max_exponent = atoi(argv[1]);
+  int method = atoi(argv[2]);   //0 if optimized, 1 if general
+  //ofilename = argv[2];
+  double *time = new double[max_exponent];
+  double *eps = new double[max_exponent];
+  //for loop here
+  for (int i = 1; i <=max_exponent; i++){
 
+    int N = (int) pow(10.0, i);
+    init(N);      //initialize constantsreturn
+    double *gt = new double[N];
+    double *dt = new double[N];
+    double *v = new double[N];
+
+    double start, end;
+    start = clock();
+    if (method){
+      cout << "Using general method" << endl;
+      forward_general(gt, dt, N);
+      backward_general(v, gt, dt, N);
+    }
+    else{
+      cout << "Using optimized method" << endl;
+      forward_optim(gt, dt, N);
+      backward_optim(v, gt, dt, N);
+    }
+
+    end = clock();
+    string expstr = to_string(i);
+
+    if (i <= 3 && method == 0){
+      string vdataname = "values";
+      vdataname.append(expstr).append(".csv");
+      ofile.open(vdataname);
+      ofile << "x," << "u," << "v" << endl;
+      for (int i = 0; i<N; i++){
+        ofile << setw(15) << setprecision(8) << x[i] <<"," << u[i] << "," << v[i] << endl;
+      }
+      ofile.close();
+    }
+
+    time[i-1] = (end - start)/CLOCKS_PER_SEC;
+    eps[i-1] = compute_eps(N, u, v);
+  }
+
+  string extradat = "epsclock";
+  extradat.append(to_string(method));
+  extradat.append(".csv");
+  ofile.open(extradat);
+  ofile << "expv," << "time," << "maxeps" <<endl;
+  for (int i = 0; i < max_exponent; i++){
+    ofile << setw(15) << setprecision(8) << to_string(i+1) << "," << time[i] << "," << eps[i] << endl;
+  }
   return 0;
 }
