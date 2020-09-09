625e7756e573e19f849353a57b32c8b2a165427e
meudnaes <eliasudnes@hotmail.com>
2020-09-09 22:01:06 +0200

    minor changes

--- Project1/vals/sol_check.py
+++ Project1/vals/sol_check.py
@@ -11,13 +11,8 @@ for exponent in [1, 2, 3]:
     sol = pd.read_csv('values'+str(exponent)+'.csv')
     gen = sol['v'].values
     LU = sol['LUv'].values
-    opt_sol = pd.read_csv('opt_values'+str(exponent)+'.csv')
-    opt = sol['v'].values
     if np.any(np.abs((gen - LU)) > 0):
         print("General algorithm and LU algorithm don't match")
         print("Maximum difference of",np.max(np.abs(gen-LU)),"in n = 10^%i" %exponent)
-    elif np.any(np.abs((gen - opt)) > 0):
-        print("General algorithm and optimized algorithm don't match")
-        print("Maximum difference of",np.max(np.abs(gen-opt)),"in n = 10^%i" %exponent)
     else:
-        print("All algorithms yielded exact same result at n = 10^%i" %exponent)
+        print("LU and general algorithms yielded exact same result at n = 10^%i" %exponent)
