fbd1d1fd1bdf70d02f9d4a0b79b54184dcafb099
meudnaes <eliasudnes@hotmail.com>
2020-09-26 08:02:48 +0200

    Small updates, rotation algorithm not yet working

--- Project2/Jacobi_rotation.cpp
+++ Project2/Jacobi_rotation.cpp
@@ -38,9 +38,72 @@ void Jacobi_rotation::print_matrix()
   m_Hamiltonian.print();
 }
 
-void Jacobi_rotation::rotate()
+void Jacobi_rotation::rotate(int n, int interact, double conv, mat a, vec r, mat v)
 {
   /* Method to rotate matrix */
+  double aip=0, aiq=0, vpi=0, vqi=0;
+  double tau=0, t=0, s=0, c=0;//tan(theta), sin(theta), cos(theta)
+  int count=1;                //count of iterations
+  int count_old=count-10;     //keep track of every 10th iteration
+  int p=n-1, q=n-2;           //off diag all same value to start
+                              //pick last as first maximum
+  clock_t start, end;
+
+  double app=a(p,p);
+  double aqq=a(q,q);
+  double apq=a(p,q);
+
+  while(abs(apq)>conv){
+      if(count>1){
+          apq=0;
+          for (int i=0;i<n;i++){
+               for (int j=0;j<n;j++){
+                  if(i!=j && abs(a(i,j))>=abs(apq)){
+                      apq=a(i,j);
+                      p=i;
+                      q=j;
+                    }
+              }
+          }
+      }
+
+      //calculate sin(theta) and cos(theta)
+      aqq=a(q,q);
+      app=a(p,p);
+      tau=(aqq-app)/(2*apq);
+      if(tau>0)
+          t=1/(tau+sqrt(1+tau*tau));
+      else
+          t=-1/(-tau+sqrt(1+tau*tau));
+      c=1/sqrt(1+t*t);
+      s=c*t;
+
+      //calculate new matrix elements and vectors
+      for(int i=0;i<n;i++){
+          if(i!=p && i!=q){
+              aip=a(i,p);
+              aiq=a(i,q);
+              a(i,p)=aip*c-aiq*s;
+              a(p,i)=aip*c-aiq*s;
+              a(i,q)=aiq*c+aip*s;
+              a(q,i)=aiq*c+aip*s;
+          }
+          //vpi=v(p,i);
+          //vqi=v(q,i);
+          vpi=v(i,p);
+          vqi=v(i,q);
+          //v(p,i)=c*vpi-s*vqi;
+         // v(q,i)=c*vqi+s*vpi;
+          v(i,p)=c*vpi-s*vqi;
+          v(i,q)=c*vqi+s*vpi;
+      }
+      a(p,p)=app*c*c-2*apq*c*s+aqq*s*s;
+      a(q,q)=app*s*s+2*apq*c*s+aqq*c*c;
+      a(p,q)=0;
+      a(q,p)=0;
+
+      count++;
+  }
 }
 
 void Jacobi_rotation::write_to_file(string filename)
