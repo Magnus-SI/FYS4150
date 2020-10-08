599f2d0228b5c49c384e21cd1a63762950fb5d36
meudnaes <eliasudnes@hotmail.com>
2020-10-08 12:28:45 +0200

    A

--- Project3/solar_system.cpp
+++ Project3/solar_system.cpp
@@ -6,10 +6,17 @@
 using namespace std;
 using namespace arma;
 
-void solar_system::initialize(int N){
-  m_N = N;
-  m_pos = zeros<mat>(m_N, m_N);
-  m_vel = zeros<mat>(m_N, m_N);
+void solar_system::initialize(str filename){
+  /*
+  Loads initial positions and velocities for the planets from a file.
+  Using	Solar System Barycenter (SSB) coordinates. Ideally finding number
+  of planets m_N from the file.
+  */
+  //m_N = len(filename);
+  m_N = 1
+  //Store pos and vel in columns because armadillo matrice is column-major
+  m_pos = zeros<mat>(3, m_N);
+  m_vel = zeros<mat>(3, m_N);
 }
 
 void solar_system::velocity_verlet(){
