b8b4920dc386c3a01e1669f38841ca727533354f
Magnus-SI <40592328+Magnus-SI@users.noreply.github.com>
2020-10-25 17:43:06 +0100

    added python file to plot and analyze results

---
+++ Project3/results.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from astropy import constants, units
+from mpl_toolkits.mplot3d import Axes3D
+
+def betaplots():
+    pass
+
+def escvelplots():
+    pass
+
+def jupiterplots():
+    pass
+
+def solarplots():
+    pass
+
+def mercury_prevession():
+    """
+    Plots mercury orbit and calculates precession?
+    """
+    data = pd.read_csv("data/mercury_2_2.00_5.00.txt")
+    pass
+
+if __name__ == "__main__":
+    #call functions here as wanted
+    pass
