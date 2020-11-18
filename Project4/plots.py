6184d2c0265c2786dd3a047abbe84b2d6cf693c6
Magnus-SI <40592328+Magnus-SI@users.noreply.github.com>
2020-11-18 07:33:59 +0100

    added first plot

--- Project4/plots.py
+++ Project4/plots.py
@@ -4,3 +4,15 @@ import pandas as pd
 
 def mcplots():
     dat = pd.read_csv("f1.csv")
+    plt.figure()
+    expvals = [0, 8, 256, 16]
+    for quant, expval in zip(dat.keys()[1:], expvals):
+
+        err = np.abs(dat[quant] - expval)
+        plt.plot(dat['mc'], err, label  = quant)
+    plt.legend()
+    plt.yscale("log")
+    plt.show()
+
+if __name__ == "__main__":
+    mcplots()
