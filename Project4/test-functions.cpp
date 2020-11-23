b7ab2f8b4d9f7d246140d2e4b2b9c2dbedb00cb9
meudnaes <eliasudnes@hotmail.com>
2020-11-23 14:38:29 +0100

    bugs

--- Project4/test-functions.cpp
+++ Project4/test-functions.cpp
@@ -6,32 +6,51 @@
 #include <iostream>
 #include "time.h"
 
+double Z(double temp, double J), E_func(double temp, double J),
+  absM_func(double temp, double J), chi_func(double temp, double J),
+  Cv_func(double temp, double J);
 
-TEST_CASE("yes"){
-  int a = 1;
+TEST_CASE("Project 4 analytic"){
+  int mcs_max = 1000000;
+  double temp = 1.0;
+  double tol = 0.0;
+  double E=0, E2=0, absM=0, M2=0;
+  ising2D test_ising;
+  test_ising.seed(-1);
+  test_ising.initialize(2, temp, tol);
+  for(int mcs=0; mcs<mcs_max; mcs++){
+    test_ising.metropolis();
+    E += test_ising.m_mean[0];
+    E2 += test_ising.m_mean[1];
+    absM+= test_ising.m_mean[4];
+    M2 += test_ising.m_mean[3];
+  }
+  E = E/mcs_max; E2 = E2/mcs_max; absM= absM/mcs_max; M2 = M2/mcs_max;
+  double C_v, chi;
+  C_v = E2 - E*E;
+  chi = M2 - absM*absM;
+  REQUIRE(E/E_func(temp, 1) == Approx(1).epsilon(1e-2));
+  REQUIRE(absM/absM_func(temp, 1) == Approx(1).epsilon(1e-2));
+  REQUIRE(C_v/Cv_func(temp, 1) == Approx(1).epsilon(1e-2));
+  REQUIRE(chi/chi_func(temp, 1) == Approx(1).epsilon(1e-2));
 }
 
+double Z(double temp, double J){
+  return 4*cosh(8*J/temp) + 12;
+}
 
-TEST_CASE("Project 3 example"){
-  int Nt = 20000;
-  double beta = 2;
-  double T = 1e9;
-  double N = 10;
-  double totE;
-  solar_system solar_solver;
-  solar_solver.initialize(N, Nt, T, beta, 0);
-  solar_solver.remove_drift();
-  solar_solver.F_G(0);
-  double* E0 = solar_solver.total_energy(0);
-  double totE0 = E0[0] - E0[1];
+double E_func(double temp, double J){
+  return -32*J/Z(temp, J)*sinh(8*J/temp);
+}
 
-  for (int i = 0; i<Nt-1; i++){
-    solar_solver.velocity_verlet(i);
-    if(remainder(i,100) == 0){
-      double* E = solar_solver.total_energy(i);
-      totE = E[0] - E[1];
-      REQUIRE(totE/totE0 == Approx(1).epsilon(1e-3));
+double absM_func(double temp, double J){
+  return -8/Z(temp, J)*(exp(8*J/temp) + 2);
+}
 
-    }
-  }
+double Cv_func(double temp, double J){
+  return 1/pow(temp, 2)*pow(32*J/Z(temp, J), 2)*(3*cosh(8*J/temp) + 1);
+}
+
+double chi_func(double temp, double J){
+  return 64/(temp*pow(Z(temp, J), 2))*(3*exp(8*J/temp) + exp(-8*J/temp) + 3);
 }
